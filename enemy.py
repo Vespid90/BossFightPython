5df538b3000b7a7c61271ba4e4c63740162acd57
Vespid <farid.elmam@gmail.com>
2025-04-11 16:10:06 +0200

    change some logic fight

--- enemy.py
+++ enemy.py
@@ -4,16 +4,16 @@ class Enemy:
     def __init__(self, name):
         self.name = name
         self.health = 3
-        self.level = 0
+        self.level = 1
         self.damage = 0
-
-    def attack(self):
-        self.damage = random.randint(self.level +0, self.level +2)
-        return self.damage
 
     def enemy_level(self, player_level):
         self.level = random.randint(player_level-1,player_level+1)
         return self.level
+
+    def attack(self):
+        self.damage = random.randint(self.level +1, self.level +2)
+        return self.damage
 
     def __str__(self):
         return f'name:{self.name}, health:{self.health}, level:{self.level}'
--- game.py
+++ game.py
@@ -9,8 +9,17 @@ def stats(pl, en):
     print("__________________________")
     print("Your stats \t\tEnemy stats")
     print("__________________________")
+    print(f"Health: {pl.health}\t\tDamage(s): {en.attack()}")
+    print(f"Level: {pl.level}\t\tLevel: {en.level}")
+    print("__________________________")
+
+def stats_now(pl, en):
+    print("__________________________")
+    print("Your stats \t\tEnemy stats")
+    print("__________________________")
     print(f"Health: {pl.health}\t\tDamage(s): {en.damage}")
     print(f"Level: {pl.level}\t\tLevel: {en.level}")
+    print("__________________________")
 
 
 class Game:
@@ -22,23 +31,27 @@ class Game:
         e = enemy
         p = player
         while p.level <= 100:
-            start = input("Enter 'a' to attack, 's' to show stats or 'l' to leave: ")
+            start = input("Enter 'a' to attack, 's' to show stats or 'q' to quit the game: ")
             if start == "a":
                 stats(p, e)
                 print("New enemy attacked")
                 if e.enemy_level(p.level) <= p.level:
+                    print("You killed the enemy ! \nYou leveled up !!")
                     p.level_up()
                     p.health_up()
-                    print("You killed the enemy ! \nYou leveled up !!")
+                    stats_now(p,e)
                 elif e.enemy_level(p.level) > p.level:
-                    p.health_lose(e.attack())
-                    print(f"Outch ! You take {e.attack()} damage(s)")
+                    p.health_lose(e.damage)
+                    stats(p,e)
+                    print(f"Outch ! You take {e.damage} damage(s)")
+                    print(f"Your life point is now at {p.health} hp")
                 elif p.health <= 0:
                     print("You lose this fight. Don't give up and restart!")
+                    stats_now(p,e)
             elif start == "s":
-                stats(p,e)
-            elif start == "l":
+                stats_now(p,e)
+            elif start == "q":
                 print("Thank you for playing ! See you soon !")
                 quit()
             else:
-                print("You need to choose between 'a', 's' or 'l' !")
+                print("You need to choose between 'a', 's' or 'q' !")
