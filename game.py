a3209e1d57c2ee2bc5740cd2169e12d6fdf38846
Vespid <farid.elmam@gmail.com>
2025-04-12 11:56:36 +0200

    add graphique and refacto logic

--- game.py
+++ game.py
@@ -9,12 +9,13 @@ enemy = Enemy("Monster")
 
 def stats(pl, en):
     print("==========================================")
-    print("Stats du Héro\t\tStats ennemi")
+    print(f"{'Stats du Héro':<26}{'Stats ennemi':<20}")
     print("==========================================")
-    print(f"Nom: {pl.name}\t\t        Nom: {en.name}")
-    print(f"Vie: {pl.health}\t\t        Dégâts: {en.attack()}")
-    print(f"Niveau: {pl.level}\t\t    Niveau: {en.level}")
+    print(f"Nom: {pl.name:<20} Nom: {en.name:<20}")
+    print(f"Vie: {pl.health:<20} Dégâts: {en.attack():<20}")
+    print(f"Niveau: {pl.level:<17} Niveau: {en.level:<20}")
     print("==========================================")
+
 
 def stats_player(pl):
     print("==========================================")
@@ -23,15 +24,6 @@ def stats_player(pl):
     print(f"Nom: {pl.name}")
     print(f"Vie: {pl.health}")
     print(f"Niveau: {pl.level}")
-    print("==========================================")
-
-def stats_enemy(en):
-    print("==========================================")
-    print("Enemy stats")
-    print("==========================================")
-    print(f"Name: {en.name}")
-    print(f"Damage(s): {en.attack()}")
-    print(f"Level: {en.level}")
     print("==========================================")
 
 def text_speed(texte, delai=0.01):
@@ -47,48 +39,51 @@ class Game:
         self.name = name
 
     def start_game(self):
-        # print("Welcome to our new RPG, BossFight! Are you ready to embark on an epic adventure?")
-        text_speed("Les brumes du monde ancien s’épaississent... \nDans les ténèbres des montagnes d’Obsydor, des créatures oubliées s’éveillent...\nToi, humble aventurier, tu as entendu l’appel.\nTon voyage commence ici.")
+        text_speed("Les brumes du monde ancien s’épaississent... \nDans les ténèbres de la forêt d’Obsydor, des créatures oubliées s’éveillent...\nToi, humble aventurier, tu as entendu l’appel.\nTon voyage commence ici.")
         print("==========================================")
         text_speed("Bienvenue dans l'univers de BossFight !")
         print("==========================================")
-        player.name = input("Quel est ton nom?: ")
+        player.name = input("Quel est ton nom?: \n")
         e = enemy
         p = player
         while p.level <= 100:
-            start = input("Entrez: \n'a' pour attaquer, \n's' pour voir les statistiques du personnage \n'q' pour quitter le jeu: ")
+            start = input("Entrez: \n'a' pour avancer dans la forêt, \n's' pour voir les statistiques du personnage \n'q' pour quitter l'ancien monde: \n")
             if start == "a":
                 if e.enemy_level(p.level) < p.level:
                     print("Un nouvel ennemi apparait ! Attention, il attaque !")
                     stats(p,e)
                     print(f"Bien joué {p.name}, tu as tué le {e.name} ! \nTu as gagné un niveau !!")
+                    print("==========================================")
                     p.level_up()
                     p.health_up()
                     continue
                 elif e.enemy_level(p.level) > p.level:
-                    print("New enemy attacked")
+                    print("Un nouvel ennemi apparait !\nAttention, il attaque !")
                     stats(p,e)
                     p.health_lose(e.damage)
                     print(f"Outch ! Le {e.name} inflige {e.damage} dégâts")
                     print(f"Tu as maintenant {p.health} points de vie")
-                    continue
-                elif p.health <= 0:
-                    stats_player(p)
-                    print("Tu es mort au combat ..")
-                    quit()
+                    if p.health <= 0:
+                        stats_player(p)
+                        print("Tu es mort au combat ..")
+                        print("==========================================")
+                        quit()
+                    else:
+                        continue
                 else:
-                    print("Un nouvel ennemi apparait ! Attention, il attaque !")
+                    print("Un nouvel ennemi apparait !\nAttention, il attaque !")
                     stats(p, e)
                     print(f"Bien joué {p.name}, tu as tué le {e.name} ! \nTu as gagné un niveau !!")
+                    print("==========================================")
                     p.level_up()
                     p.health_up()
                     continue
-                    # stats(p,e)
-                    # print("error")
             elif start == "s":
                 stats_player(p)
             elif start == "q":
                 print("Merci d'avoir joué ! A bientôt!")
+                print("==========================================")
                 quit()
             else:
                 print("Vous devez choisir entre 'a', 's' ou 'q' !")
+                print("==========================================")
