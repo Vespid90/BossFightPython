61d0ae6b0317b0f200c1cced8589c881a68e9b37
Vespid <farid.elmam@gmail.com>
2025-04-16 15:01:51 +0200

    systeme de foret + alea

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from character import Player
 from character import Monster
 from stats import Stats
 from text import Text
+import random
 
 player = Player("Hero")
 monster = Monster("Monster")
@@ -26,25 +27,37 @@ class Game:
             start = input("Quelle action souhaites-tu faire ?: \n'a' pour avancer dans la forêt, \n's' pour voir les statistiques du personnage \n'q' pour quitter l'ancien monde: \n")
             if start == "a":
                 print("================================================")
-                print("Un nouvel ennemi apparait !")
-                stats.stats(p, m)
-                if m.monster_level(p.level) > p.level:
-                    p.health_lose(m.damage)
-                    print(f"Outch ! Le {m.name} inflige {m.damage} dégâts")
-                    print(f"Tu as maintenant {p.health} points de vie")
-                    print("================================================")
-                    if p.health <= 0:
-                        print("Tu es mort au combat")
-                        stats.stats_player(p)
-                        quit()
+                alea = random.randint(0, 3)
+                if alea == 0:
+                    print("Un nouvel ennemi apparait !")
+                    stats.stats(p, m)
+                    if m.monster_level(p.level) > p.level:
+                        p.health_lose(m.damage)
+                        print(f"Outch ! Le {m.name} inflige {m.damage} dégâts")
+                        print(f"Tu as maintenant {p.health} points de vie")
+                        print("================================================")
+                        if p.health <= 0:
+                            print("Tu es mort au combat")
+                            stats.stats_player(p)
+                            quit()
+                        else:
+                            continue
                     else:
+                        print(f"Bien joué {p.name}, tu as tué le {m.name} ! \nTu as gagné un niveau !!")
+                        p.level_up()
+                        p.health_up()
+                        print(f"Tu es maintenant niveau {p.level} et tu as {p.health} points de vie")
+                        print("================================================")
                         continue
+                elif alea == 1:
+                    print("Vous êtes tombé sur un piège ! vous perdez 3 points de vie")
+                    p.health(3)
+                elif alea == 2:
+                    pass
+                elif alea == 3:
+                    print("Vous avez trouvé un objet !")
                 else:
-                    print(f"Bien joué {p.name}, tu as tué le {m.name} ! \nTu as gagné un niveau !!")
-                    p.level_up()
-                    p.health_up()
-                    print(f"Tu es maintenant niveau {p.level} et tu as {p.health} points de vie")
-                    print("================================================")
+                    print("Vous avancez dans la forêt sombre")
                     continue
             elif start == "s":
                 stats.stats_player(p)
